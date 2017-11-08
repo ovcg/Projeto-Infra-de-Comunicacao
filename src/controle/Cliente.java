04f73d93e61bd19a006bf8afa66ebc1ad5c4de0a
Otávio <otavioveracruz@gmail.com>
2017-11-08 15:05:58 -0300

    Atualizando Projeto

--- src/controle/Cliente.java
+++ src/controle/Cliente.java
@@ -1,7 +1,19 @@
 package controle;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+
+import base.Pacote;
+
 public class Cliente extends Thread {
-	
-	
+
+	private Pacote pacote;
+	private BufferedInputStream bis;
+	private BufferedOutputStream bos;
+
+	@Override
+	public void run() {
+
+	}
 
 }
--- src/gui/TelaInicial.java
+++ src/gui/TelaInicial.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controle.Cliente;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -15,18 +18,20 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Color;
 
-public class TelaPrincipal extends JFrame implements Serializable {
+public class TelaInicial extends JFrame implements Serializable {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
 	private String path;
+	private String nomeArquivo;
 	private JLabel lblNomeDoArquivo;
 	private JLabel lblEndereoDestino;
 	private JLabel lblPortaDestino;
 	private JLabel lblTamanhoDoArquivo;
 	private JLabel lblTransfer;
 	private JLabel lblPortaParaRtt;
+	private Cliente cliente;
 
 	/**
 	 * Launch the application.
@@ -35,7 +40,7 @@ public class TelaPrincipal extends JFrame implements Serializable {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TelaPrincipal frame = new TelaPrincipal();
+					TelaInicial frame = new TelaInicial();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -47,76 +52,76 @@ public class TelaPrincipal extends JFrame implements Serializable {
 	/**
 	 * Create the frame.
 	 */
-	public TelaPrincipal() {
+	public TelaInicial() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 666, 425);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.setColumns(10);
 		textField.setBounds(172, 80, 300, 19);
 		contentPane.add(textField);
-		
-		
+
 		JButton button = new JButton("Enviar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				TelaEnviando enviar = new TelaEnviando(cliente);
+				enviar.setVisible(true);
+			}
+		});
 		button.setBounds(27, 327, 117, 25);
 		contentPane.add(button);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(172, 153, 300, 19);
 		contentPane.add(textField_1);
-		
+
 		JButton btnNewButton = new JButton("Escolher Arquivo");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				JFileChooser file=new JFileChooser();
+				JFileChooser file = new JFileChooser();
 				int abrir = file.showOpenDialog(null);
-				if(abrir==JFileChooser.APPROVE_OPTION) {
-					
+				if (abrir == JFileChooser.APPROVE_OPTION) {
+					path = file.getSelectedFile().getAbsolutePath();
+					nomeArquivo = file.getName();
+					lblNomeDoArquivo.setText("Nome do Arquivo: "+nomeArquivo);
+					lblTamanhoDoArquivo.setText("Tamanho do Arquivo: "+file.getSelectedFile().getTotalSpace()+"Bytes");
 				}
+
 			}
 		});
 		btnNewButton.setBounds(27, 230, 163, 25);
 		contentPane.add(btnNewButton);
-		
+
 		lblNomeDoArquivo = new JLabel("Nome do arquivo: ");
 		lblNomeDoArquivo.setBounds(233, 235, 348, 15);
 		contentPane.add(lblNomeDoArquivo);
-		
+
 		lblEndereoDestino = new JLabel("Endereço destino:");
 		lblEndereoDestino.setBounds(25, 82, 188, 15);
 		contentPane.add(lblEndereoDestino);
-		
+
 		lblPortaDestino = new JLabel("Porta destino:");
 		lblPortaDestino.setBounds(25, 155, 102, 15);
 		contentPane.add(lblPortaDestino);
-		
+
 		lblTamanhoDoArquivo = new JLabel("Tamanho do Arquivo:");
 		lblTamanhoDoArquivo.setBounds(27, 282, 249, 15);
 		contentPane.add(lblTamanhoDoArquivo);
-		
+
 		lblTransfer = new JLabel("Transfer");
 		lblTransfer.setForeground(Color.GREEN);
 		lblTransfer.setBounds(27, 27, 163, 60);
-		lblTransfer.setSize(100,50);
+		lblTransfer.setSize(100, 50);
 		contentPane.add(lblTransfer);
-		
+
 		lblPortaParaRtt = new JLabel("Porta para RTT:");
 		lblPortaParaRtt.setBounds(27, 195, 117, 15);
 		contentPane.add(lblPortaParaRtt);
 	}
-	private static class __Tmp {
-		private static javax.swing.JTextField textField_2;
-		private static void __tmp() {
-			  javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();
-			  
-			  textField_2 = new javax.swing.JTextField();
-			  __wbp_panel.add(textField_2);
-			  textField_2.setColumns(10);
-		}
-	}
+
 }
