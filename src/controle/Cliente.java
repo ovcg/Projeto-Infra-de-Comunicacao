047d4919de2d4e979ce243bf8f6fb09e2ba600aa
Otavio Vera Cruz Gomes <ovcg@windows.cin.ufpe.br>
2017-11-24 17:30:12 -0300

    Add server

--- src/controle/Cliente.java
+++ src/controle/Cliente.java
@@ -4,11 +4,10 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.net.Socket;
 
-import base.Pacote;
 
 public class Cliente extends Thread {
 
-	private Pacote pacote;
+	
 	private BufferedInputStream bis;
 	private BufferedOutputStream bos;
 	private Socket socket;
